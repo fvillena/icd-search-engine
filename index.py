a5c2af64ab9e017ccacdb8d791e92bf1504205fc
Fabián Villena <villenafabian@gmail.coma>
2022-11-09 09:11:51 -0300

    initial commit

---
+++ index.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import whoosh.fields
+import whoosh.index
+import whoosh.qparser
+import whoosh.analysis
+import whoosh.support.charset
+import whoosh.scoring
+import os
+import csv
+
+
+my_analyzer = whoosh.analysis.LanguageAnalyzer("es") | whoosh.analysis.CharsetFilter(whoosh.support.charset.accent_map)# | whoosh.analysis.NgramFilter(minsize=2, maxsize=4)
+
+
+
+schema = whoosh.fields.Schema(
+    code = whoosh.fields.ID(stored=True),
+    description = whoosh.fields.TEXT(stored=True,analyzer=my_analyzer),
+    description_additional = whoosh.fields.TEXT(stored=False,analyzer=my_analyzer)
+)
+
+
+if not os.path.exists("index"):
+    os.mkdir("index")
+ix = whoosh.index.create_in("index", schema)
+
+writer = ix.writer()
+
+
+with open('icd.csv', newline='', encoding="iso-8859-1") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=";")
+    for row in reader:
+        writer.add_document(code=row["code"],description=row["description"])
+
+
+writer.commit()
---
+++ main.py
@@ -0,0 +1,37 @@
+import fastapi
+import whoosh.index
+import whoosh.qparser
+import json
+
+with open("priors.json") as p:
+    priors = json.load(p)
+
+app = fastapi.FastAPI(title="ICD Search Engine", description="API for searching in ICD", version="1.0")
+
+ix = whoosh.index.open_dir("index")
+parser = whoosh.qparser.MultifieldParser(["description"], ix.schema, group=whoosh.qparser.OrGroup.factory(0.9))
+searcher = ix.searcher()
+
+@app.get("/")
+async def search_cieom(q: str):  
+    myquery = parser.parse(q)
+    results = searcher.search(myquery, limit=10, terms=True)
+    response = []
+    for result in results:
+        hit = {
+            "code":result["code"],
+            "description":result["description"],
+            "score":result.score,
+        }
+        
+        if hit["code"] in priors:
+            hit["prior"] = priors[hit["code"]]
+        else:
+            hit["prior"] = None
+            
+        response.append(hit)
+    return response
+
+@app.on_event('shutdown')
+def shutdown():
+    searcher.close()
